a01fdf2e9da3be755c7cd0f854be3e28c959d050
AlexanderChiclana <alexander_chiclana@alumni.brown.edu>
2018-08-10 14:25:07 -0400

    cant figure out dynamic update

--- assets/scripts/bucket/bucketEvents.js
+++ assets/scripts/bucket/bucketEvents.js
@@ -12,38 +12,38 @@ const bucketUi = require('./bucketUI')
 //    .then(bucketUi.getBooksSuccess)
 //    .catch(bucketUi.failure)
 
-const onGetItems = function(event) {
-    event.preventDefault()
-    bucketApi.getItems()
-        // .then(bucketUi.getItemsSuccess)
-        // .catch(bucketUi.onError)
+const onGetItems = function (event) {
+  event.preventDefault()
+  bucketApi.getItems()
+  // .then(bucketUi.getItemsSuccess)
+  // .catch(bucketUi.onError)
 }
 
 
-const onShowItem = function(event) {
-    event.preventDefault()
-    const data = getFormFields(event.target)
-    // could put some input validation here if we want
-    // message the user if validation fails
-    bucketApi.showItem(data)
-        // .then(bucketUi.showItemSuccess)
-        // .catch(bucketUi.onError)
+const onShowItem = function (event) {
+  event.preventDefault()
+  const data = getFormFields(event.target)
+  // could put some input validation here if we want
+  // message the user if validation fails
+  bucketApi.showItem(data)
+  // .then(bucketUi.showItemSuccess)
+  // .catch(bucketUi.onError)
 }
 
 
-const onDeleteItem = function(event) {
-    event.preventDefault()
-    const itemId = $(event.target).closest('ul').attr('data-id')
-    bucketApi.deleteItem(itemId)
-    //   .then(bucketUi.onDeleteItemSuccess)
-    //   .catch(bucketUi.onError)
+const onDeleteItem = function (event) {
+  event.preventDefault()
+  const itemId = $(event.target).closest('ul').attr('data-id')
+  bucketApi.deleteItem(itemId)
+  //   .then(bucketUi.onDeleteItemSuccess)
+  //   .catch(bucketUi.onError)
 }
 
 
 // const onUpdateItem = function (event) {
- //   event.preventDefault()
- //   const data = getFormFields(event.target)
- //   // could put some input validation here if we want
+//   event.preventDefault()
+//   const data = getFormFields(event.target)
+//   // could put some input validation here if we want
 //    // message the user if validation fails
 //    bucketApi.updateItem(data)
 //        // .then(bucketUi.onUpdateItemSuccess)
@@ -52,17 +52,17 @@ const onDeleteItem = function(event) {
 
 
 const onCreateItem = function (event) {
-    event.preventDefault()
-    const data = getFormFields(event.target)
-    // could put some input validation here if we want
-    // message the user if validation fails
+  event.preventDefault()
+  const data = getFormFields(event.target)
+  // could put some input validation here if we want
+  // message the user if validation fails
 
-    //bucketApi.createItem(data)
-        // .then(bucketUi.onCreateItemSuccess)
-        // .catch(bucketUi.onError)
+  //bucketApi.createItem(data)
+  // .then(bucketUi.onCreateItemSuccess)
+  // .catch(bucketUi.onError)
 
-    // make API call
-    bucketApi.createItem(data)
+  // make API call
+  bucketApi.createItem(data)
     // if API call is successful, call a success function in UI
     .then(() => console.log(data))
     // if API call fails, call an error function in UI
@@ -84,7 +84,7 @@ const onDeleteBook = (event) => {
   event.preventDefault()
   const bookId = $(event.target).closest('div').attr('data-id')
   bucketApi.deleteBook(bookId)
-    .then(() => onGetBooks(event))
+    .then(() => onGetBlItems(event))
     .catch(bucketUi.failure)
 }
 
@@ -93,25 +93,26 @@ const onUpdateItem = function (event) {
   const data = getFormFields(event.target)
   console.log('logging update')
   console.log(data)
+  console.log(this)
   // bucketApi.updateItem(data)
   // .then(bucketUi.onUpdateItemSuccess)
   // .catch(bucketUi.onError)
 
 }
 const addHandlers = () => {
-    // for each of the following replace the spaces between the sets of single quotes:
-    // add the HTML id, class, or element we want to attach an event to, and the event/s
-    // optional: add a selector and/or data before the event/s
-    // $(' ').on(' ', onGetItems)
-    // $(' ').on(' ', onShowItem)
-    // $(' ').on(' ', onDeleteItem)
-    // $(' ').on(' ', onUpdateItem)
-    $('#create-item').on('submit', onCreateItem)
+  // for each of the following replace the spaces between the sets of single quotes:
+  // add the HTML id, class, or element we want to attach an event to, and the event/s
+  // optional: add a selector and/or data before the event/s
+  // $(' ').on(' ', onGetItems)
+  // $(' ').on(' ', onShowItem)
+  // $(' ').on(' ', onDeleteItem)
+  // $(' ').on(' ', onUpdateItem)
+  $('#create-item').on('submit', onCreateItem)
 
-    $('#getBooksButton').on('click', onGetBlItems)
+  $('#getBooksButton').on('click', onGetBlItems)
   $('#clearBooksButton').on('click', onClearBooks)
   $('.content').on('click', '.btn-danger', onDeleteBook)
-  $('.content').on('click', '.btn-secondary', onUpdateItem)
+  $('.content').on('submit', '.btn-secondary', onUpdateItem)
 
 }
 
