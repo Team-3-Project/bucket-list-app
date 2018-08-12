3addd8f21d85b4bac8d6d798abe528e5a7cd1550
AlexanderChiclana <alexander_chiclana@alumni.brown.edu>
2018-08-12 15:28:39 -0400

    removed redundant calls

--- assets/scripts/bucket/bucketEvents.js
+++ assets/scripts/bucket/bucketEvents.js
@@ -8,26 +8,6 @@ const bucketUi = require('./bucketUI')
 
 
 
-// const onShowItem = function (event) {
-//   event.preventDefault()
-//   const data = getFormFields(event.target)
-//   // could put some input validation here if we want
-//   // message the user if validation fails
-//   bucketApi.showItem(data)
-//   // .then(bucketUi.showItemSuccess)
-//   // .catch(bucketUi.onError)
-// }
-
-
-const onDeleteItem = function (event) {
-  event.preventDefault()
-  const itemId = $(event.target).closest('ul').attr('data-id')
-  bucketApi.deleteItem(itemId)
-  //   .then(bucketUi.onDeleteItemSuccess)
-  //   .catch(bucketUi.onError)
-}
-
-
 // const onUpdateItem = function (event) {
 //   event.preventDefault()
 //   const data = getFormFields(event.target)
