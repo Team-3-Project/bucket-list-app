1bf9ea9598d3f490d1cbbc42099fd53096a0ff9c
RiverMagnetic <magneticcontinuum@gmail.com>
2018-08-12 18:30:40 -0400

    Clean up file

--- assets/scripts/bucket/bucketEvents.js
+++ assets/scripts/bucket/bucketEvents.js
@@ -4,10 +4,6 @@ const getFormFields = require('../../../lib/get-form-fields')
 const bucketApi = require('./bucketApi')
 const bucketUi = require('./bucketUI')
 
-
-
-
-
 // const onShowItem = function (event) {
 //   event.preventDefault()
 //   const data = getFormFields(event.target)
@@ -18,43 +14,14 @@ const bucketUi = require('./bucketUI')
 //   // .catch(bucketUi.onError)
 // }
 
-
-const onDeleteItem = function (event) {
-  event.preventDefault()
-  const itemId = $(event.target).closest('ul').attr('data-id')
-  bucketApi.deleteItem(itemId)
-  //   .then(bucketUi.onDeleteItemSuccess)
-  //   .catch(bucketUi.onError)
-}
-
-
-// const onUpdateItem = function (event) {
-//   event.preventDefault()
-//   const data = getFormFields(event.target)
-//   // could put some input validation here if we want
-//    // message the user if validation fails
-//    bucketApi.updateItem(data)
-//        // .then(bucketUi.onUpdateItemSuccess)
-//        // .catch(bucketUi.onError)
-//    }
-
-
 const onCreateItem = function (event) {
   event.preventDefault()
   const data = getFormFields(event.target)
   // could put some input validation here if we want
   // message the user if validation fails
-
-  //bucketApi.createItem(data)
-  // .then(bucketUi.onCreateItemSuccess)
-  // .catch(bucketUi.onError)
-
-  // make API call
   bucketApi.createItem(data)
-    // if API call is successful, call a success function in UI
     .then(() => console.log(data))
     .then(bucketUi.onCreateItemSuccess)
-    // if API call fails, call an error function in UI
     .catch(bucketUi.failure)
 }
 
@@ -81,6 +48,8 @@ const onDeleteBlItem = (event) => {
 const onUpdateItem = function (event) {
   event.preventDefault()
   const data = getFormFields(event.target)
+  // could put some input validation here if we want
+  // message the user if validation fails
   console.log('logging update')
   console.log(data)
   console.log(this)
@@ -90,20 +59,17 @@ const onUpdateItem = function (event) {
 
 }
 const addHandlers = () => {
-  // for each of the following replace the spaces between the sets of single quotes:
-  // add the HTML id, class, or element we want to attach an event to, and the event/s
-  // optional: add a selector and/or data before the event/s
-  // $(' ').on(' ', onGetItems)
-  // $(' ').on(' ', onShowItem)
-  // $(' ').on(' ', onDeleteItem)
-  // $(' ').on(' ', onUpdateItem)
-  $('#create-item').on('submit', onCreateItem)
 
+  // If we want to use this JQuery, replace the spaces between the sets of single quotes:
+  // add the HTML id, class, or element we want to attach an event to, and the event/s
+  // optional: add a selector and/or data before the event/s:
+  // $(' ').on(' ', onShowItem)
+
+  $('#create-item').on('submit', onCreateItem)
   $('#getBlItemsButton').on('click', onGetBlItems)
   $('#clearBlItemsButton').on('click', onClearBlItems)
   $('.content').on('click', '.btn-danger', onDeleteBlItem)
   $('.content').on('click', '.btn-secondary', onUpdateItem)
-
   $('.updater').on('submit', onUpdateItem)
 
 }
