ab1583169256bb2ad8040c64838c027eb4d33fbe
RiverMagnetic <magneticcontinuum@gmail.com>
2018-08-12 16:32:22 -0400

    Clean up authUi

--- assets/scripts/auth/authUi.js
+++ assets/scripts/auth/authUi.js
@@ -1,21 +1,12 @@
 // 'use strict'
 const store = require('../store')
 
-const resetForms = function () {
-  document.getElementById('sign-up').reset()
-  document.getElementById('sign-in').reset()
-  document.getElementById('change-password').reset()
-
-}
-
 const onSignUpSuccess = function () {
-  $('#sign-up-message').text('Signed up successfully.')
-  $('#sign-up-message').css('background-color', 'green')
+
 }
 
 const onSignUpFailure = function () {
-  $('#sign-up-message').text('Signed up failed.')
-  $('#sign-up-message').css('background-color', 'red')
+
 }
 
 
@@ -23,24 +14,19 @@ const onSignInSuccess = function (data) {
   store.user = data.user
   $('#loginPage').css('display', 'none')
   $('#mainPage').css('display', 'block')
-  $('#sign-in-message').text('Signed in successfully.')
-  $('#sign-in-message').css('background-color', 'green')
   $('#getBlItemsButton').click()
 
 }
 const onSignInFailure = function (error) {
-  $('#sign-in-message').text('Signed in failed.')
-  $('#sign-in-message').css('background-color', 'red')
+
 }
 
 const onChangePasswordSuccess = function () {
-  $('#change-password-message').text('Changed password successfully.')
-  $('#change-password-message').css('background-color', 'green')
+
 }
 
 const onChangePasswordFailure = function () {
-  $('#change-password-message').text('Password change failed.')
-  $('#change-password-message').css('background-color', 'red')
+
 }
 
 const onSignOutSuccess = function () {
@@ -50,8 +36,7 @@ const onSignOutSuccess = function () {
 }
 
 const onSignOutFailure = function () {
-  $('#sign-out-message').text('Signed out failed.')
-  $('#sign-out-message').css('background-color', 'red')
+
 }
 
 module.exports = {
