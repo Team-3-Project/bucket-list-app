2e7de8ecf71666e3cd9e26cfb3747bd5d2d158b6
cymbiotica <terry.esposito@gmail.com>
2018-08-13 13:01:52 -0400

    url update again

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://boiling-harbor-16682.herokuapp.com/',
+  production: 'https://boiling-harbor-16682.herokuapp.com',
   development: 'http://localhost:4741'
   // commented out for handlebars practice
   // development: 'https://ga-library-api.herokuapp.com'
