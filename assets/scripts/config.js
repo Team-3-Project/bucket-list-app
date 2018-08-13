281976b43ffa79fd94ad6e9c93ad3ef13a997a91
cymbiotica <terry.esposito@gmail.com>
2018-08-12 22:50:47 -0400

    blooming url slashes

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://team-3-project.github.io/bucket-list-app/',
+  production: 'https://team-3-project.github.io/bucket-list-app',
   development: 'http://localhost:4741'
   // commented out for handlebars practice
   // development: 'https://ga-library-api.herokuapp.com'
